00de32daae26a6be0a78cbd64826a469b4b5ee98
Pawan Gujral <gujral87@gmail.com>
2026-04-03 19:58:42 +0200

    fix: remove basePath for custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/pawangujral.dev",
-  assetPrefix: "/pawangujral.dev/",
 };
 
 export default nextConfig;
